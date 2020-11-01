d6ee74e1c2e0f787894a080bff95c20e354d72cf
Lmt <m18476049965@163.com>
2020-11-01 22:52:02 +0800

    修改hello文件

--- src/com/mt/git/Hello.java
+++ src/com/mt/git/Hello.java
@@ -8,4 +8,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public void test() {
+        System.out.println("test()");
+    }
 }
